7d021ccc2f3f69b7582bca0b99fcb810491fcad8
johnluetz <johnluetz@utexas.edu>
2019-07-14 00:28:49 -0500

    first bit of tkinter

--- checkIn.py
+++ checkIn.py
@@ -1,16 +1,36 @@
 #script for checking in students for their advising appointments
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
-import sys
+from tkinter import *
 
+class Window(Frame):
 
-if __name__ == '__main__':
-    
-    app = QApplication(sys.argv)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
 
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-    
-    sys.exit(app.exec_())
+    def init_window(self):
+        self.master.title("YEET") #title of master
+        self.pack(fill = BOTH, expand = 1) #widget takes full space
+        timeButton = Button(self, text="SELECT", command=self.client_exit) #create a button
+        timeButton.place(x=85, y=23) #place button
+
+    def client_exit(self):
+        exit()
+
+#basic GUI code     
+
+root = Tk()
+root.geometry("400x300") #size of window
+app = Window(root)
+tkvar = StringVar(root)
+choices = {'yadda','yeet','boys'}
+tkvar.set('yadda')
+popupMenu = OptionMenu(app, tkvar, *choices)
+Label(app, text="Select a time").grid(row = 1, column = 1)
+popupMenu.grid(row = 2, column =1)
+
+def change_dropdown(*args):
+    print(tkvar.get())
+
+root.mainloop() #runs last!
+
